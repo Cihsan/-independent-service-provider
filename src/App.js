c10f451a7d13fc9e3c39855fe5a796dccd95b8fe
Cihsan <mutamuty@gmail.com>
2022-04-18 07:40:08 +0600

    added RequireAuth set protected path

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './Pages/Unique/Home';
 import Login from './Pages/Unique/Login';
 import NotFound from './Pages/Unique/NotFound';
 import SignUp from './Pages/Unique/SignUp';
+import RequireAuth from './RequireAuth';
 
 function App() {
   return (
@@ -16,22 +17,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/about' element={
+        <Route path='/about' element={<RequireAuth><About></About></RequireAuth>}></Route>
 
-        <About></About>
-        
-        }></Route>
+        <Route path='/blogs' element={<RequireAuth><Blogs></Blogs></RequireAuth>}></Route>
 
-        <Route path='/blogs' element={
-
-        <Blogs></Blogs>
+        <Route path='/checkout' element={<RequireAuth><Checkout></Checkout></RequireAuth>}></Route>
         
-        }></Route>
-        <Route path='/checkout' element={
-        
-        <Checkout></Checkout>
-        
-        }></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
         <Route path='/*' element={<NotFound></NotFound>}></Route>
